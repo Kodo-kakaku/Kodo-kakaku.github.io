5addc270c7acfdf7f5b37e7d8ed26af15e1910c8
Vladislav <spanky@yandex.ru>
2024-06-12 19:47:04 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          I want to learn how to use React for the user interface in Tauri + Rust) I am a backend developer using Spring Java
         </p>
       </header>
     </div>
